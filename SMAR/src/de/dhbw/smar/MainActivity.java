8a8efee8b7468590a3118ba9a931047c173181ce
Raffael <raffael.wojtas@web.de>
2015-03-21 11:39:53 +0100

    Initialcommit
    Change-Id: I26db65612e96f169cc3280a7dd3a32bbd1b5a119
    Signed-off-by: Raffael <raffael.wojtas@web.de>

--- SMAR/src/de/dhbw/smar/MainActivity.java
+++ SMAR/src/de/dhbw/smar/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends Activity {
         }
     }
 
-
+    //Das ist ein Testkommentar.
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
