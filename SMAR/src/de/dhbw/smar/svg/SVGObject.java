c518d6e4cd9cc638efc949c4df182c21ffe0611e
pianomister <stegiemail-github@yahoo.de>
2015-05-28 00:27:38 +0200

    Update SVGObject: add marked sections functionality and save calculations for static shelves
    Change-Id: I6f7d4f1bcc1bd41e83c5bb5c5cd6c08e2f287054
    Signed-off-by: pianomister <stegiemail-github@yahoo.de>

--- SMAR/src/de/dhbw/smar/svg/SVGObject.java
+++ SMAR/src/de/dhbw/smar/svg/SVGObject.java
@@ -7,34 +7,50 @@ import android.graphics.drawable.PictureDrawable;
 
 import com.caverock.androidsvg.SVG;
 import com.caverock.androidsvg.SVGParseException;
-import com.caverock.androidsvg.SVGParser;
 
 public class SVGObject implements Serializable {
 
 	private static final long serialVersionUID = 4208154811234L;
 	private String svg;
+	private Drawable drawable;
 	
 	/**
 	 * Constructor
 	 * @param String svgDocument containing an SVG document
 	 */
 	public SVGObject(String svgDocument) {
-		svg = svgDocument;
+		this.setSVG(svgDocument);
 	}
 
+	/**
+	 * sets new SVG Document for this object
+	 * @param svgDocument
+	 */
+	public void setSVG(String svgDocument) {
+		svg = svgDocument;
+		drawable = null;
+	}
 	
+	/**
+	 * Generate / return drawable object for displaying in imageView
+	 * @return Drawable Drawable for the given SVG
+	 */
 	public Drawable getDrawable() {
 
-        SVG graphic;
-		try {
-			// Parse the SVG file from string
-			graphic = SVG.getFromString(svg);
-			// Get a drawable from the parsed SVG
-	        return new PictureDrawable(graphic.renderToPicture());
-		} catch (SVGParseException e) {
-			e.printStackTrace();
-	        return null;
+		if(this.drawable == null) {
+			try {
+				
+				// Parse the SVG file from string
+				SVG graphic = SVG.getFromString(svg);
+				// Get a drawable from the parsed SVG
+				this.drawable = new PictureDrawable(graphic.renderToPicture());
+
+			} catch (SVGParseException e) {
+				e.printStackTrace();
+		        return null;
+			}
 		}
+		return drawable;
 	}
 	
 	
@@ -47,18 +63,23 @@ public class SVGObject implements Serializable {
 	 */
 	public Drawable getDrawable(int sectionID) {
 
-		// TODO
+		String section        = "section" + sectionID + "\" class=\"section\"";
+		String sectionReplace = "section" + sectionID + "\" class=\"section selected\"";
+		String text           = "section" + sectionID + "-text\"";
+		String textReplace    = "section" + sectionID + "-text\" class=\"textselected\"";
+		String svgMarked = svg;
+		svgMarked = svgMarked.replace(section, sectionReplace);
+		svgMarked = svgMarked.replace(text, textReplace);
 		
-        SVG graphic;
 		try {
 			// Parse the SVG file from string
-			graphic = SVG.getFromString(svg);
+			SVG graphic = SVG.getFromString(svgMarked);
 			// Get a drawable from the parsed SVG
-	        return new PictureDrawable(graphic.renderToPicture());
+			return new PictureDrawable(graphic.renderToPicture());
 		} catch (SVGParseException e) {
 			e.printStackTrace();
-	        return null;
 		}
+		return null;
 	}
 	
 	
